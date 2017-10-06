3ecbea0f7995c38c4c8855285d4ee43e4d02716e
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-06 11:02:12 -0600

    completed

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,13 @@
 class LaughTracksApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get "/comedians" do
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else
+      @comedians = Comedian.all
+    end
+    erb :index
+  end
+
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,10 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
+
+  def mean_age
+    mean = Comedian.average(:age)
+    mean.to_i
+  end
+
 
 end
